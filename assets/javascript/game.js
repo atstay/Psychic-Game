96ab97d32f40673fdb8c970b7b31f99753905e21
Shubalub Kingz <atstay@gmail.com>
2018-06-10 23:13:19 -0600

    HW3 Final Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,26 @@
 
-// Define alphabet letters that computer can pick from
+// Array of alphabet letters for the user to choose from
 var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Set the initial global variables
+
+// Global variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-// guessesSoFar is an array that will hold all the user's guesses in each round
+var guessesLeft = 7;
+
+// Array that will hold user's guesses
 var guessesSoFar = [];
-// userGuess is what the user picks by pressing a key
+
+// What the user's selected so far
 var userGuess = null;
-// Have computer pick a letter and store it in letterToBeGuessed
+
+// Randomly selected letters by computer
 var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
 console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 
-// start listening for events
+// Game listens for user to press key
 document.onkeyup = function(event) {
 
-	// When user presses a key, it records it and saves to userGuess
+	// When user presses a key, it records it to userGuess
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 	// Add the user's guess to guessesSoFar array but 
@@ -30,31 +34,38 @@ document.onkeyup = function(event) {
 	}
 
 	// if letterToBeGuessed is same as userGuess then record it as a win
-	// and then reset guessesLeft to 9, and empty the guessesSoFar array
+	// and then reset guessesLeft to 7, and empty the guessesSoFar array
 	// also have the computer make a new random pick
 	if (letterToBeGuessed == userGuess) {
 		wins++;
-		console.log("You won!");
-		guessesLeft = 9;
+		document.getElementById('mario').play();
+		window.alert ("You won! TAKE THESE GYPSY TEARS");
+		guessesLeft = 7;
 		guessesSoFar = [];
 		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
-		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 	}
 
 	// if guessesLeft gets to 0 then record it as a loss
-	// and then reset guessesLeft to 9, and empty the guessesSoFar array
+	// and then reset guessesLeft to 7, and empty the guessesSoFar array
 	// also have the computer make a new random pick
 	if (guessesLeft == 0) {
 		losses++;
-		console.log("You lost!");
-		guessesLeft = 9;
+		document.getElementById('cackle').play();
+		window.alert ("You lost! GIVE ME YOUR TEARS");
+		guessesLeft = 7;
 		guessesSoFar = [];
 		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
-		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 	}
 
 	// Displaying progress to HTML
-	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+    var html = 
+    "<p><h1>The Psychic Gypsy Game</h1></p>" + 
+    "<p><h4>Guess What Letter I\'m Thinking Of:</h4></p>" + 
+    "<p><h4>Wins: " + wins + "</h4></p>" + 
+    "<p><h4>Losses: " + losses + "</h4></p>" + 
+    "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + 
+    "<p><h4>Your Guesses So Far: " + guessesSoFar + "</h4></p>";
+    "<br>";
 	// place html into the game ID
 	document.querySelector("#game").innerHTML = html;
 
